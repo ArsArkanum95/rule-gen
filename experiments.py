90343b1bb683185e603d1ead9759835e7601567d
ArsArkanum95 <m.jozwiak@iambot.ai>
2019-05-06 16:27:16 +0200

    Very minor experiments update

--- experiments.py
+++ experiments.py
@@ -1,16 +1,18 @@
 from stage_generator import generate_stage
 
 
-def run_experiments():
-    duration = 100
+def run_experiments(*, duration, **stage_gen_kwargs):
     while True:
-        stage = generate_stage(5, 0, 2, duration)
+        stage = generate_stage(max_time=duration, **stage_gen_kwargs)
 
         sequence = stage.generate(duration)
         if not sequence:
             continue
 
         event_coverage, rule_coverage = stage.analyse(sequence, duration)
+        print(event_coverage)
+
+        res = []
 
         for rule_id, rule in enumerate(stage.rules):
             rule_test_results = rule_coverage[rule_id]
@@ -20,8 +22,14 @@ def run_experiments():
             test_probability, test_p = rule_test_results
             true_probability = rule.probability
             
-            print(true_probability, test_probability, test_p)
+            res.append((true_probability, test_probability, test_p))
 
+        true_probabilities, test_probabilities, test_p_values = list(zip(*res))
         return
 
-run_experiments()
+run_experiments(
+    num_rules=10,
+    max_aggregation_level=0,
+    num_nodes=2,
+    duration=100
+)
